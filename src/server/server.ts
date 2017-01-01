a67b1a6f018acbea6df43a30d549ef82b31e8f93
CoderCat84 <robert.goins@gmail.com>
2017-01-01 12:02:31 -0500

    adding temp html file while I set up angular2

--- src/server/server.ts
+++ src/server/server.ts
@@ -2,9 +2,12 @@ import * as express from "express";
 
 let app = express();
 
+app.use(express.static("dist"));
+
 app.get('/', (req, res) => {
     res.send("Hello World");
 });
 
 app.listen(process.env.PORT || 8080, () => {
+    console.log("Press Ctrl+C to Quit");
 });
