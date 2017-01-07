9983bd7433b29bb031a71414586a83eed40c7765
CoderCat84 <robert.goins@gmail.com>
2017-01-07 15:51:02 -0500

    according to the firebase docs its okay to add these publicly

---
+++ src/client/api-keys.ts
@@ -0,0 +1,9 @@
+export const keys = {
+    firebase: {
+    apiKey: "AIzaSyBXMzY-Jdwcz7butUCwhHlqQL0Ye2SW1zM",
+    authDomain: "buglet-154218.firebaseapp.com",
+    databaseURL: "https://buglet-154218.firebaseio.com",
+    storageBucket: "buglet-154218.appspot.com",
+    messagingSenderId: "771717310706"
+  }
+}
